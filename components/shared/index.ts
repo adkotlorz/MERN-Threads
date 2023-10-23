b6f9d3bcdd7ab14169d69fb9157868b82b5a428a
adkotlorz <adkotlorz@gmail.com>
2023-10-24 00:42:21 +0200

    basic structure done

---
+++ components/shared/index.ts
@@ -0,0 +1,11 @@
+import RightSidebar from "./RightSidebar";
+import LeftSidebar from "./LeftSidebar";
+import TopBar from "./TopBar";
+import BottomBar from "./BottomBar";
+
+export {
+  RightSidebar,
+  LeftSidebar,
+  TopBar,
+  BottomBar,
+};
