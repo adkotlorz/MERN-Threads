49c2d10a49f56467b91da5ad21549a5ae0560df9
adkotlorz <adkotlorz@gmail.com>
2023-10-24 16:38:26 +0200

    comment section done

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -77,3 +77,85 @@ export const fetchPosts = async (
 
   return { posts, isNext };
 };
+
+export const fetchThreadById = async (
+  id: string
+) => {
+  connectToDB();
+
+  //TODO Populate communities
+  try {
+    const thread = await Thread.findById(id)
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id id name image",
+      })
+      .populate({
+        path: "children",
+        populate: [
+          {
+            path: "author",
+            model: User,
+            select: "_id name parentId image",
+          },
+          {
+            path: "children",
+            model: Thread,
+            populate: {
+              path: "author",
+              model: User,
+              select: "_id name parentId image",
+            },
+          },
+        ],
+      })
+      .exec();
+
+    return thread;
+  } catch (error: any) {
+    throw new Error(
+      `Error fetching thread ${error.message}`
+    );
+  }
+};
+
+export const addCommentToThread = async (
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) => {
+  connectToDB();
+
+  try {
+    const originalThread = await Thread.findById(
+      threadId
+    );
+
+    if (!originalThread) {
+      throw new Error("Could not find thread");
+    }
+
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
+    });
+
+    const savedCommentThread =
+      await commentThread.save();
+
+    originalThread.children.push(
+      savedCommentThread._id
+    );
+
+    await originalThread.save();
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(
+      `Error adding comment to thread: ${error.message}`
+    );
+  }
+};
