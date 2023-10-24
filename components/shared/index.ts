e5c9f33be617a8aa0c877b6c5af165bfe97c3b98
adkotlorz <adkotlorz@gmail.com>
2023-10-24 19:09:55 +0200

    profile tab already working

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -2,10 +2,14 @@ import RightSidebar from "./RightSidebar";
 import LeftSidebar from "./LeftSidebar";
 import TopBar from "./TopBar";
 import BottomBar from "./BottomBar";
+import ProfileHeader from "./ProfileHeader";
+import ThreadsTab from "./ThreadsTab";
 
 export {
   RightSidebar,
   LeftSidebar,
   TopBar,
   BottomBar,
+  ProfileHeader,
+  ThreadsTab,
 };
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 interface updateUserProps {
   userId: string;
@@ -62,6 +63,36 @@ export const fetchUser = async (
   } catch (error: any) {
     throw new Error(
       `Failed to fetch user: ${error.message}`
+    );
+  }
+};
+
+export const fetchUserPosts = async (
+  userId: string
+) => {
+  try {
+    connectToDB();
+
+    const threads = await User.findOne({
+      id: userId,
+    }).populate({
+      path: "threads",
+      model: Thread,
+      populate: {
+        path: "children",
+        model: Thread,
+        populate: {
+          path: "author",
+          model: User,
+          select: "name image id",
+        },
+      },
+    });
+
+    return threads;
+  } catch (error: any) {
+    throw new Error(
+      `Failed to fetch user posts ${error.message}`
     );
   }
 };
